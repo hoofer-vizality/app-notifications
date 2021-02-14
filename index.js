4c94a1d5a25006eedc31c27800ecfec9f00db701
Hunter <60201971+HooferDevelops@users.noreply.github.com>
2021-02-14 15:52:44 -0500

    ( •̀ ω •́ )✧
    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import { Plugin } from '@vizality/entities';
+import { patch, unpatch } from '@vizality/patcher';
+import { getModule } from '@vizality/webpack';
+
+const HeaderComponent = require("./components/HeaderComponent")
+
+export default class MentionUtilities extends Plugin {
+    async start () {
+        this.injectStyles('style.scss');  
+        // modules
+        const makeTextChatNotification = getModule(["makeTextChatNotification"], false);
+        const MessageContent = getModule(m => m.type?.displayName == "MessageContent", false);
+        const transition = getModule(["transitionTo"], false);
+        const parseTopic = await getModule(["parseTopic"], true);
+        const parser = parseTopic.parse;
+
+        // injectors
+        patch("notif-display", makeTextChatNotification, "makeTextChatNotification", (args,res) => {
+            var toastId = `app-notif-${Date.now()^12345}`;
+            vizality.api.notices.sendToast(toastId, {
+                header: <HeaderComponent avatar={res.icon} text={res.title}/>,
+                timeout: 5000,
+                content: <MessageContent.type message={{...args[1], hasFlag: () => false, isEdited: () => false}} content={parser(args[1].content, true, { channelId: args[0].id })}/>,
+                buttons:[{
+                    text: "Jump to Message",
+                    size: "small",
+                    color: "green",
+                    look: "filled",
+                    onClick: () => {
+                        transition.transitionTo(`/channels/${args[0].guild_id ? args[0].guild_id : `@me`}/${args[1].channel_id}/${args[1].id}`);
+                        vizality.api.notices.closeToast(toastId);
+                    }
+                },
+                {
+                    text: "Close Notification",
+                    size: "small",
+                    look: "ghost",
+                    onClick: () => {
+                        vizality.api.notices.closeToast(toastId);
+                    }
+                }]
+            })
+            return res;
+        });
+    }
+
+    stop () {
+        unpatch("notif-display")
+    }
+}
