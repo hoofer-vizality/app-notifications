ed1bf53b5231d4f4c6bb480b0369c55a572db5bd
Jason <52143580+Swishilicous@users.noreply.github.com>
2021-03-21 01:25:21 -0700

    fixed for new changes
    i also cleaned up some of the code because it was unbearable

--- components/HeaderComponent.jsx
+++ components/HeaderComponent.jsx
@@ -1,15 +1,21 @@
-import { React, getModule, getModuleByDisplayName, contextMenu } from "@vizality/webpack"
-import { Modal, Icon, Button, SearchBar, Anchor, Divider, Text, Avatar } from "@vizality/components"
+import React from "react";
 
+import { Text, Avatar } from "@vizality/components";
 
-module.exports = class HeaderComponent extends React.PureComponent {
-    constructor(props){
-        super(props)
-    }
-
-    render() {
-        return <>
-            <Text style={{"margin-bottom": "5px"}}><Avatar className="an-avatar" src={this.props.avatar} isTyping={false} isMobile={false} status="online" statusTooltip={false} size={Avatar.Sizes.SIZE_16} />{this.props.text}</Text>
-        </>
-    }
+export default ({ avatar, text }) => {
+    console.log(avatar)
+    return <>
+        <Text style={{"margin-bottom": "5px"}}>
+            <Avatar 
+                className="an-avatar" 
+                src={avatar} 
+                isTyping={false} 
+                isMobile={false} 
+                status="online" 
+                statusTooltip={false} 
+                size={Avatar.Sizes.SIZE_16}
+            />
+            {text}    
+        </Text>
+    </>
 }
